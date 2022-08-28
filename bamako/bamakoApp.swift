9da88cf92f64b0728a54888786b39b46719dcb2d
Sexy-Red-Wolf <98813918+Sexy-Red-Wolf@users.noreply.github.com>
2022-08-28 22:39:22 +0200

    Initial Commit

---
+++ bamako/bamakoApp.swift
@@ -0,0 +1,17 @@
+//
+//  bamakoApp.swift
+//  bamako
+//
+//  Created by Emma GAUBERT on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct bamakoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
